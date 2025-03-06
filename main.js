4e1fbd3c434a3e11f3a20aeba0731895abcb36cf
Yasht14j <findyasht@gmail.com>
2025-03-06 14:53:13 -0500

    initialized drop and takes in json files and extracts data from the files needed for followers and following

--- main.js
+++ main.js
@@ -27,7 +27,74 @@ const initApp = () => {
     });
 
 
+    droparea.addEventListener('drop', handleDrop)
+    
+    
+    
+    
+    function handleDrop(event) {
+    event.preventDefault();
+
+    const files = event.dataTransfer.files;
+
+    if (files.length === 0){
+        console.error("Please upload a zip file");
+        return;
+    }
+
+    const file = files[0];
+
+    if (!file.name.endsWith('.zip')) {
+        console.error("Uploaded file is not a zip");
+        return;
+    }
+
+    if (files.length > 0){
+        const file = files[0];
+
+        
+        JSZip.loadAsync(file).then(function(zip){
+            console.log(zip);
+            console.log("ZIP File Contents:", Object.keys(zip.files));
+            const requiredfiles = ['following.json', 'followers_1.json'];
+            const foundFiles = {};
+            
+
+            Object.keys(zip.files).forEach(filePath => { 
+                requiredfiles.forEach(reqFile => {
+                    const filePathS = String(filePath);
+                    const normalizedPath = filePath.replace(/\\/g, '/');
+                    if(normalizedPath.endsWith(`/${reqFile}`) || filePathS === reqFile){
+                        foundFiles[reqFile] = filePath;
+                    }
+                });
+            });
+    
+            requiredfiles.forEach(fileName =>{
+                if(foundFiles[fileName]){
+                    zip.files[foundFiles[fileName]].async('text').then(function(content){
+                        try{
+                            const jsonObject = JSON.parse(content);
+                            console.log(jsonObject)
+                        }
+                        catch (err) {
+                            console.error('error parsing json', err);
+                        }
+                    }).catch (function(err){
+                        console.error('error reading file in zip', err);
+                    })
+                } else{
+                    console.error(`${fileName} not found in zip`);
+                } 
+            })
+        }).catch (function(err){
+            console.error("error loading the zip file");
+        })
+    } else{
+        console.error("please upload zip file");
+    }
+}
 
 }
 
-document.addEventListener("DOMContentLoaded", initApp)
+document.addEventListener("DOMContentLoaded", initApp);
